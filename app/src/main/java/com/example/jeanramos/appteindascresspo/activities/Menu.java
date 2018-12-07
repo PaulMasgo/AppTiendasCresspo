6f9330dac43158dace7d798d91fc0222feb62163
fredy122 <i1413470@continental.edu.pe>
2018-12-06 21:00:40 -0500

    Commit numero 3

--- app/src/main/java/com/example/jeanramos/appteindascresspo/activities/Menu.java
+++ app/src/main/java/com/example/jeanramos/appteindascresspo/activities/Menu.java
@@ -7,23 +7,34 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.jeanramos.appteindascresspo.MainActivity;
 import com.example.jeanramos.appteindascresspo.R;
 import com.example.jeanramos.appteindascresspo.generals.Settings;
 
 public class Menu extends AppCompatActivity {
 
     private Button btnProductos;
+    private Button btnFormulario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
+        btnFormulario = (Button) findViewById(R.id.btnNosotros);
         btnProductos = (Button) findViewById(R.id.btnProductos);
         btnProductos.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Log.i(Settings.INFO, "Products");
                 Intent i = new Intent(Menu.this,ListaProductos.class);
+                startActivity(i);
+            }
+        });
+
+        btnFormulario.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.i(Settings.INFO, "Products");
+                Intent i = new Intent(Menu.this, RegisterActivity.class);
                 startActivity(i);
             }
         });
